e954029f42fd603bb191dc089cec1a0c3aa5bd3a
李权 <11483845+kingsleyesovo@user.noreply.gitee.com>
2024-07-11 01:12:26 +0800

    "change"

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
+	github.com/volcengine/volcengine-go-sdk v1.0.147
 )
 
 require (
@@ -18,6 +19,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
@@ -28,6 +31,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/volcengine/volc-sdk-golang v1.0.23 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
--- hello.go
+++ hello.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/volcengine/volcengine-go-sdk/service/arkruntime"
+	"github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
+	"github.com/volcengine/volcengine-go-sdk/volcengine"
 )
 
 func main() {
+	// getInstance
 	server := gin.Default()
+
+	//set cors
 	server.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
@@ -23,40 +32,77 @@ func main() {
 	server.GET(("/chat"), func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "hello"})
 	})
+	server.POST("/chat", func(c *gin.Context) {
 
+		var params struct {
+			Text string `json:"text"`
+		}
+		c.ShouldBindJSON(&params)
+		fmt.Printf(params.Text)
+		client := arkruntime.NewClientWithApiKey(
+			os.Getenv("ARK_API_KEY"),
+			arkruntime.WithBaseUrl("https://ark.cn-beijing.volces.com/api/v3"),
+			arkruntime.WithRegion("cn-beijing"),
+		)
+		ctx := context.Background()
+		// use if err... to replace the try catch
+		req := model.ChatCompletionRequest{
+			Model: "ep-20240707141643-75bdr",
+			Messages: []*model.ChatCompletionMessage{
+				{
+					Role: model.ChatMessageRoleSystem,
+					Content: &model.ChatCompletionMessageContent{
+						StringValue: volcengine.String("你是豆包，是由字节跳动开发的 AI 人工智能助手"),
+					},
+				},
+				{
+					Role: model.ChatMessageRoleUser,
+					Content: &model.ChatCompletionMessageContent{
+						StringValue: volcengine.String(params.Text),
+					},
+				},
+			},
+		}
+		resp, err := client.CreateChatCompletion(ctx, req)
+		fmt.Println(*resp.Choices[0].Message.Content.StringValue)
+		if err != nil {
+
+			fmt.Printf("standard chat error: %v\n", err)
+			return
+		}
+		// fmt.Println(*resp.Choices[0].Message.Content.StringValue)
+		// stream, err := client.CreateChatCompletionStream(ctx, req)
+		// if err != nil {
+		// 	fmt.Printf("stream chat error: %v\n", err)
+		// 	return
+		// }
+		// defer stream.Close()
+
+		// for {
+		// 	recv, err := stream.Recv()
+		// 	if err == io.EOF {
+		// 		return
+		// 	}
+		// 	if err != nil {
+		// 		fmt.Printf("Stream chat error: %v\n", err)
+		// 		return
+		// 	}
+
+		// 	if len(recv.Choices) > 0 {
+		// 		fmt.Print(recv.Choices[0].Delta.Content)
+		// 	}
+		// }
+		if err := c.ShouldBindJSON(&params); err != nil {
+			fmt.Println("绑定错误信息:", err)
+			c.JSON(400, gin.H{
+				"error": "invalid params",
+			})
+			return
+		}
+		c.JSON(200, gin.H{"success": "OK", "content": params.Text, "answer": resp.Choices[0].Message.Content})
+	})
 	server.Run(":9000")
-	// client := arkruntime.NewClientWithApiKey(
-	// 	os.Getenv("ARK_API_KEY"),
-	// 	arkruntime.WithBaseUrl("https://ark.cn-beijing.volces.com/api/v3"),
-	// 	arkruntime.WithRegion("cn-beijing"),
-	// )
 
-	// ctx := context.Background()
-
-	// fmt.Println("----- standard request -----")
-	// req := model.ChatCompletionRequest{
-	// 	Model: "ep-20240707141643-75bdr",
-	// 	Messages: []*model.ChatCompletionMessage{
-	// 		{
-	// 			Role: model.ChatMessageRoleSystem,
-	// 			Content: &model.ChatCompletionMessageContent{
-	// 				StringValue: volcengine.String("你是豆包，是由字节跳动开发的 AI 人工智能助手"),
-	// 			},
-	// 		},
-	// 		{
-	// 			Role: model.ChatMessageRoleUser,
-	// 			Content: &model.ChatCompletionMessageContent{
-	// 				StringValue: volcengine.String("请问什么是厘米的猜想"),
-	// 			},
-	// 		},
-	// 	},
-	// }
-
-	// resp, err := client.CreateChatCompletion(ctx, req)
-	// if err != nil {
-	// 	fmt.Printf("standard chat error: %v\n", err)
-	// 	return
-	// }
 	// fmt.Println(*resp.Choices[0].Message.Content.StringValue)
 
 	// fmt.Println("----- streaming request -----")
@@ -77,25 +123,5 @@ func main() {
 	// 		},
 	// 	},
 	// }
-	// stream, err := client.CreateChatCompletionStream(ctx, req)
-	// if err != nil {
-	// 	fmt.Printf("stream chat error: %v\n", err)
-	// 	return
-	// }
-	// defer stream.Close()
 
-	// for {
-	// 	recv, err := stream.Recv()
-	// 	if err == io.EOF {
-	// 		return
-	// 	}
-	// 	if err != nil {
-	// 		fmt.Printf("Stream chat error: %v\n", err)
-	// 		return
-	// 	}
-
-	// 	if len(recv.Choices) > 0 {
-	// 		fmt.Print(recv.Choices[0].Delta.Content)
-	// 	}
-	// }
 }
